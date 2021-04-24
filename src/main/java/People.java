72cf026137a50c79ab6b9fdde223aa02c8185b87
hazel <liam.k.becker@gmail.com>
2021-04-24 15:26:21 -0400

    Checkpointing

--- src/main/java/People.java
+++ src/main/java/People.java
@@ -1,2 +1,23 @@
-public abstract class People {
+import java.util.List;
+
+public abstract class People<PersonType extends Person> implements Iterable<PersonType> {
+
+    private List<PersonType> personList;
+
+    public People(List<PersonType> personList){
+        this.personList = personList;
+    }
+
+    public void add(PersonType person){
+        personList.add(person);
+    }
+
+    public void remove(PersonType person){
+        personList.remove(person);
+    }
+
+    public int size(){
+        return this.personList.size();
+    }
+
 }
